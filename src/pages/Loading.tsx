4179880c61ebbbda408f87ed16af6f00acc17562
seoin <seoin1002@mement.ai>
2024-10-26 14:21:18 +0900

    style: 로딩 css 수정

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -9,6 +9,6 @@ export default function Loading() {
     }, 2000);
 
   return (
-     isLoading ? (<div className="w-full h-full bg-black flex justify-center items-center animate-fadeIn"><img src="/image/beautyTech.png" alt="logo" className="w-48" /></div>) : <RootLayout />
+     isLoading ? (<div className="w-full h-full bg-black flex justify-center items-center animate-fadeIn"><img src="/image/beautyTech.png" alt="logo" className="w-60" /></div>) : <RootLayout />
   )
 }
