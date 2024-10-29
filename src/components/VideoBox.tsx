3e00793d86a30e76fc77af89821fb33e9c3f41c6
seoin <seoin1002@mement.ai>
2024-10-29 09:59:12 +0900

    style: 모바일 반응형 비디오 박스 폰트 크기 수정

--- src/components/VideoBox.tsx
+++ src/components/VideoBox.tsx
@@ -9,10 +9,10 @@ export default function VideoBox({videoLink, firstTitle, secondTitle}: IVideoBox
     <section className="relative bg-gray-500 h-[960px] mobile:h-[537.8px] flex items-center">
         <video src={videoLink} autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover z-0 mobile:h-[537.8px]" />
         <div className="overflow-hidden w-[740px] h-[162px] flex flex-col justify-center mobile:justify-center gap-4 items-left z-10 object-cover mx-auto mobile:ml-8 w-[75%]">
-            <p className="text-white text-left text-[64px] font-medium animate-fadeInLeft whitespace-nowrap w-[55%] mobile:text-[26px]">
+            <p className="text-white text-left text-[64px] font-medium animate-fadeInLeft whitespace-nowrap w-[55%] mobile:text-[22px]">
       {firstTitle || "Tech Company"}
             </p>
-            <p className="text-white text-left text-[64px] font-medium animate-fadeInRight -mt-6 whitespace-nowrap w-[40%] mobile:text-[26px]">
+            <p className="text-white text-left text-[64px] font-medium animate-fadeInRight -mt-6 whitespace-nowrap w-[40%] mobile:text-[22px]">
       {secondTitle || "running Beauty business"}
             </p>
     </div>
